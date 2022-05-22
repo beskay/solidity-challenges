e4741c57abdf22fd19057e771d3bd4529ca6cc4d
beskay <btsk@protonmail.com>
2022-05-22 23:56:47 +0200

    added more functions to coldstorage contract

--- src/ColdStorage.sol
+++ src/ColdStorage.sol
@@ -1,16 +1,56 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+/**
+ * General purpose cold storage contract
+ * Implements a call and delegatecall function
+ * delegate is a timelock contract, where its possible
+ * to lock up your ETH for a given duration, basically
+ * forcing you to hodl
+ */
 contract ColdStorage {
     address public delegate;
     address public owner;
+
+    event Deposit(address _from, uint256 value);
+    event DelegateUpdated(address oldDelegate, address newDelegate);
 
     constructor(address _imp) {
         owner = msg.sender;
         delegate = _imp;
     }
 
-    function _delegate(address _imp) internal {
+    modifier onlyAuth() {
+        require(
+            msg.sender == owner || msg.sender == address(this),
+            "No permission"
+        );
+        _;
+    }
+
+    fallback() external payable {
+        if (msg.value > 0) emit Deposit(msg.sender, msg.value);
+        else if (msg.data.length > 0) _delegate(delegate);
+    }
+
+    function execute(address _target, bytes memory payload)
+        external
+        returns (bytes memory)
+    {
+        (bool success, bytes memory ret) = address(_target).call(payload);
+        require(success, "failed");
+        return ret;
+    }
+
+    function upgradeDelegate(address newDelegateAddress) external {
+        require(msg.sender == owner);
+        address oldDelegate = delegate;
+        delegate = newDelegateAddress;
+
+        emit DelegateUpdated(oldDelegate, newDelegateAddress);
+    }
+
+    function _delegate(address _imp) internal onlyAuth {
         assembly {
             // Copy msg.data. We take full control of memory in this inline assembly
             // block because it will not return to Solidity code. We overwrite the
@@ -33,14 +73,5 @@ contract ColdStorage {
                 return(0, returndatasize())
             }
         }
-    }
-
-    function upgradeDelegate(address newDelegateAddress) public {
-        require(msg.sender == owner);
-        delegate = newDelegateAddress;
-    }
-
-    fallback() external payable {
-        if (msg.data.length > 0) _delegate(delegate);
     }
 }
