94bfa5b36996f923d28de6786270fe668da783c1
beskay <btsk@protonmail.com>
2022-06-01 17:33:24 +0200

    added deployment script

--- README.md
+++ README.md
@@ -10,7 +10,26 @@
 
 ## Install
 
+Install [Foundry](https://github.com/gakonst/foundry) if you haven't already.
+
 ```
 git clone git@github.com:beskay/solidity-challenges.git
 git submodule update --init --recursive  ## initialize submodule dependencies
 ```
+
+## Deploy and verify
+
+To deploy the contracts, run
+
+```
+forge script ./scripts/deploy.sol --tc Deploy --rpc-url $ETH_RPC_URL --broadcast --private-key $PK -vvv
+```
+
+Note that ETH_RPC_URL and PK has to be set
+
+```
+export ETH_RPC_URL=<your_etherscan_apikey>
+export PK=<your_private_key>
+```
+
+To verify the deployed contracts, see [here](https://book.getfoundry.sh/forge/deploying.html?highlight=verify#verifying-a-pre-existing-contract)
---
+++ scripts/deploy.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+
+import {PrivateData} from "../src/PrivateData.sol";
+import {Wallet} from "../src/Wallet.sol";
+import {WalletLibrary} from "../src/WalletLibrary.sol";
+import {ColdStorage} from "../src/ColdStorage.sol";
+import {Timelock} from "../src/Timelock.sol";
+import {EtherWallet} from "../src/EtherWallet.sol";
+import {VNFT} from "../src/VNFT.sol";
+
+contract Deploy is Test {
+    // Contracts
+    PrivateData internal privatedata;
+    WalletLibrary internal walletLibrary;
+    Wallet internal wallet;
+    ColdStorage internal coldstorage;
+    Timelock internal lock;
+    EtherWallet internal etherwallet;
+    VNFT internal vnft;
+
+    function run() public {
+        vm.startBroadcast();
+
+        // deploy privatedata
+        privatedata = new PrivateData();
+
+        // deploy wallet & walletlibrary
+        address[] memory _owners = new address[](3);
+        _owners[0] = vm.addr(1);
+        _owners[1] = vm.addr(2);
+        _owners[2] = vm.addr(3);
+        walletLibrary = new WalletLibrary();
+        wallet = new Wallet(address(walletLibrary), _owners, 2);
+
+        // deploy coldstorage & timelock
+        lock = new Timelock();
+        coldstorage = new ColdStorage(address(lock));
+
+        // deploy and set up etherwallet
+        etherwallet = new EtherWallet();
+
+        // deploy vnft
+        vnft = new VNFT();
+
+        vm.stopBroadcast();
+    }
+}
